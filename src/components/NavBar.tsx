5d4b499498c84fea5c70063fbefb492f81c3c1e9
Miguel <miguel.martinez.mestre@alumnos.upm.es>
2020-11-09 04:34:22 +0100

    style-components

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default function NavBar(){
+return (
+    <nav class="menu">
+    <ul>
+        <li><a href="#">Inicio</a></li>
+    <li><a href="#">Sobre nosotros</a></li>
+    <li><a href="#">Contacto</a></li>
+    </ul>
+    </nav>
+    )
+}
